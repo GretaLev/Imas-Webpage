fc670d3d768b4912f800ba616aa0857eb61c5500
GretaLev <levinskaite.greta@gmail.com>
2023-12-02 16:36:42 +0200

    add heroSection component

---
+++ src/components/HeroSection.js
@@ -0,0 +1,18 @@
+const HeroSection = () => {
+  return (
+    <div className="container">
+      <div className="d-flex flex-column align-items-center">
+        <p>We turn digital dreams into reality</p>
+        <p>
+          We design unique websites, efficient e-commerce solutions,
+          user-friendly mobile apps, stylish UI/UX, and advanced business
+          management systems. Our creative innovation and technical excellence
+          bring ideas to life, empowering clients in the digital realm.
+        </p>
+        <p>TIMER</p>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
