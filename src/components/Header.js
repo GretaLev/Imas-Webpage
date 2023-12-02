4a9eafdfc367763887e9d8caa16059c38f150033
GretaLev <levinskaite.greta@gmail.com>
2023-12-02 14:58:25 +0200

    add header component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import Logo from "../assets/logo.svg";
+
+import styles from "./Header.module.scss";
+
+const Header = () => {
+  return (
+    <div className={[styles.header, "container"].join(" ")}>
+      <div className="d-flex flex-row justify-content-between align-items-center mt-4">
+        <img src={Logo} alt="company-logo"></img>
+        <button className={styles.btnBrand}>Get in touch</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
